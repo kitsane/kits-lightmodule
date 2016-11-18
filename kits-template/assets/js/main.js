384edc11fb037d4ba2c6da03fd88b98d61fc35d0
Andi Staub <andi.staub@gmail.com>
2016-11-18 14:59:16 +0100

    Slow down image switch in slider

--- kits-template/assets/js/main.js
+++ kits-template/assets/js/main.js
@@ -1,7 +1,8 @@
 $(function() {
   var mySwiper = new Swiper ('.swiper-container', {
     loop: true,
-    autoplay: 2500,
+    autoplay: 5000,
+    duration: 1000,
     pagination: '.swiper-pagination',
     paginationClickable: true,
   })
